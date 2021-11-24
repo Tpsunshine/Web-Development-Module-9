dd93aeaab545a3a5eba92226611edddf1cb65021
Tsumshine <tpdawsun@gmail.com>
2021-11-24 10:01:18 +0200

    Module 9 Assignment

---
+++ readfile.js
@@ -0,0 +1,25 @@
+$(document).ready(function () {
+    $("#AJ").click(function () {
+        $("#text1").load("./history/APJ.txt");
+    });
+});
+
+$(document).ready(function () {
+    $("#RT").click(function () {
+        $("#text2").load("./history/RT.txt");
+    });
+});
+
+
+$(document).ready(function () {
+    $("#MT").click(function () {
+        $("#text3").load("./history/MT.txt");
+    });
+});
+
+$(document).ready(function () {
+    $("#NM").click(function () {
+        $("#text4").load("./history/NM.txt");
+    });
+});
+
